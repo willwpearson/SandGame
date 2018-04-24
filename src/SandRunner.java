1b3cd95967b18c5f5cf5d28e47d7b82dbeed0449
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-04-24 13:37:28 -0600

    Added a quick for loop to make it all one element.

--- src/SandRunner.java
+++ src/SandRunner.java
@@ -2,7 +2,7 @@ public class SandRunner
 {
    public static void main(String [] args)
    {
-      SandLab app = new SandLab(50, 50); 
+      SandLab app = new SandLab(250, 250); 
       app.run();
    }
   
